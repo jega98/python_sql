1afc4849c6845e843b216ca8108dc490c221a7d6
jega98 <104147847+jega98@users.noreply.github.com>
2022-05-01 19:43:35 +0530

    insert endpoint

--- app.py
+++ app.py
@@ -11,9 +11,30 @@ def get_customers():
     db = 'my schema details'
     db = sql.connect(host = host, user = user, password = password, port = port, db = db)
     c=db.cursor()
+      
     c.execute("""select * from customers""") 
     final_result = c.fetchall()
-    num_fields = len(c.description)
+    field_names = [i[0] for i in c.description]
+    return {"data": final_result, "coloum": field_names}
+
+@app.route('/customers/insert')
+def insert_customers():
+    host = 'localhost'
+    user = 'root'
+    password = 'sivakavi'
+    port = 3306
+    db = 'my schema details'
+    db = sql.connect(host = host, user = user, password = password, port = port, db = db)
+    c=db.cursor()
+    c.executemany(
+          """INSERT INTO customers (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, addressLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit)
+      VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+      [
+      (573, "jevanathan", "ram", "jey", "9345470683", "sugunaStore", "annaNagar", "madurai", "tamilNadu", "cscsc x", "india", 1165, 1000000),
+      ] )
+      
+    c.execute("""select * from customers""") 
+    final_result = c.fetchall()
     field_names = [i[0] for i in c.description]
     return {"data": final_result, "coloum": field_names}
 
