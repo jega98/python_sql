0a100849028c5253da4aa0f9efaedc82b837777c
jega98 <104147847+jega98@users.noreply.github.com>
2022-05-05 08:58:48 +0530

    select orders using orderID endpoint

--- app.py
+++ app.py
@@ -103,6 +103,21 @@ def get_employee(employeeID):
     field_names = [i[0] for i in c.description]
     return {"data": final_result, "coloum": field_names}
 
+@app.route('/orders/<int:orderID>')
+def get_order(orderID):
+    host = 'localhost'
+    user = 'root'
+    password = 'sivakavi'
+    port = 3306
+    db = 'my schema details'
+    db = sql.connect(host = host, user = user, password = password, port = port, db = db)
+    c=db.cursor()
+      
+    c.execute("""select * from orders where orderNumber ="""+str(orderID)) 
+    final_result = c.fetchall()
+    field_names = [i[0] for i in c.description]
+    return {"data": final_result, "coloum": field_names}
+
 
 if __name__ == '__main__':
    app.run()
