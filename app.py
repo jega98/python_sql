2697d36f87fed9e8c6daea54cd1679e8b0949439
jega98 <104147847+jega98@users.noreply.github.com>
2022-05-05 08:43:23 +0530

    employee with employeeID endpoint

--- app.py
+++ app.py
@@ -88,5 +88,21 @@ def get_customer(customerID):
     field_names = [i[0] for i in c.description]
     return {"data": final_result, "coloum": field_names}
 
+@app.route('/employees/<int:employeeID>')
+def get_employee(employeeID):
+    host = 'localhost'
+    user = 'root'
+    password = 'sivakavi'
+    port = 3306
+    db = 'my schema details'
+    db = sql.connect(host = host, user = user, password = password, port = port, db = db)
+    c=db.cursor()
+      
+    c.execute("""select * from employees where employeeNumber ="""+str(employeeID)) 
+    final_result = c.fetchall()
+    field_names = [i[0] for i in c.description]
+    return {"data": final_result, "coloum": field_names}
+
+
 if __name__ == '__main__':
    app.run()
